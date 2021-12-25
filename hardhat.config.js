06f0fe382c1912084f3a2666eca3753a32fb4f78
Filali Med Amar <medamarfilali@gmail.com>
2021-12-25 21:16:57 +0100

    Adding the NFT smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
     },
     mumbai: {
       url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
-      accounts: [],
+      accounts: [process.env.PRIVATE_KEY],
     },
     mainnet: {
       url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
-      accounts: [],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
   solidity: "0.8.4",
