65e684119c0f264b20fee124a623d4fc4f8c3717
Filali Med Amar <medamarfilali@gmail.com>
2021-12-25 20:56:30 +0100

    Initialization and configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,18 @@
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${process.env.PROJECT_ID}`,
+      accounts: [],
+    },
+  },
   solidity: "0.8.4",
 };
