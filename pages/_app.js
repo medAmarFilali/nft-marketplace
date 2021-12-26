c137bc9526a5dc333ded391a4a7934c7eae57009
Filali Med Amar <medamarfilali@gmail.com>
2021-12-26 10:31:51 +0100

    Added navigation menu

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Taxas Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-indigo-500 hover:text-indigo-600">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-indigo-500 hover:text-indigo-600">
+              Sell Digital Assets
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-indigo-500 hover:text-indigo-600">
+              My Digital Assets
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-indigo-500 hover:text-indigo-600">
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
