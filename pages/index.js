cc4f6da4742839e6ff62274750eed7c7b84aadeb
Filali Med Amar <medamarfilali@gmail.com>
2021-12-26 12:29:11 +0100

    Adding the home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,105 @@
-import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import { nftMarketAddress, nftAddress } from "../config";
+
+import NFT from "../artifacts/contracts/NFT.sol/NFT.json";
+import Market from "../artifacts/contracts/NFTMarket.sol/NFTMarket.json";
 
 export default function Home() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  const loadNFTs = async () => {
+    const provider = new ethers.providers.JsonRpcProvider();
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider);
+    const marketContract = new ethers.Contract(
+      nftMarketAddress,
+      Market.abi,
+      provider
+    );
+    const data = await marketContract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await tokenContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  };
+
+  const buyNft = async (nft) => {
+    const web3model = new Web3Modal();
+    const connection = await web3model.connect();
+    const provider = new ethers.providers.web3Provider(connection);
+
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(mftMarketAddress, Market.abi, signer);
+
+    const price = ethers.utils.parseUnits(nft.price.toString(), "ether");
+
+    const transaction = await contract.createMarketSale(
+      nftAddress,
+      nft.tokenId,
+      { value: price }
+    );
+
+    await transaction.wait();
+
+    loadNFTs();
+  };
+
+  if (loadingState == "loaded" && !nfts.length) {
+    return (
+      <h1 className="px-20 py-10 text-3xl">No items in the marketplace</h1>
+    );
+  }
   return (
-    <div className={styles.container}>
-      <h1 className="flex items-center h-screen justify-center text-3xl text-gray-700">
-        Home
-      </h1>
+    <div className="flex justify-center">
+      <div className="px-4" style={{ maxWidth: "1600px" }}>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4">
+          {nfts.map((nft, i) => (
+            <div key={i} className="border shadow rounded-xl overflow-hidden">
+              <img src={nft.image} alt={nft.name} />
+              <div className="p-4">
+                <p className="h-[64px] text-2xl font-semibold">{nft.name}</p>
+                <div className="h-[70px] overflow-hidden">
+                  <p className="text-gray-700">{nft.description}</p>
+                </div>
+              </div>
+              <div className="p-4 bg-black">
+                <p className="text-2xl mb-4 font-bold text-white">
+                  {nft.price} Matic
+                </p>
+                <button
+                  onClick={() => buyNft(nft)}
+                  className="w-full bg-indigo-500 text-white font-bold py-2 px-12 rounded"
+                >
+                  Buy
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
